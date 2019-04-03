e0f6eef51cc67e6bee7b89a2d679be3371073fa1
James <42484571+smitteh1@users.noreply.github.com>
2019-04-03 08:23:31 -0400

    Update Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int fibNumbers = 20;
+
+int fib(int n) {
+  if (n == 0)
+    return 0;
+    else if (n == 1)
+        return 1;
+        else return fib(n-1) + fib(n-2);
+}
+
+int main() {
+  for (int x = 0; x < fibNumbers; x++)
+    cout << fib(x) << " , ";
+
+  cout << endl;
+  return 0;
+}
