0b1bab5759023ef300e809b073c1c9347e66cac2
James <42484571+smitteh1@users.noreply.github.com>
2019-04-02 21:49:30 -0400

    Added Commenting and added some other things.

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -3,29 +3,36 @@
 
 using namespace std;
 
+// Declaring Structs
 struct Parent;
 struct Child;
 struct Family;
 
+// Person Object - almost all of our objects will be build off of this
 struct Person {
   public:
   string name;
   int age;
+  // used to pass onto children
   string toString;
 
+  // Constructor
   Person(string nm, int page) :
   name(nm), age(page)  {}
 };
 
+// Family Object
 struct Family {
   public:
   std::list<Person> members;
   string lastname;
 
+  // welcome to the family
   void addMember(Person m) {
     members.push_back(m);
   }
 
+  // Compiles all of the members toStrings
   string toString() {
     string buildon = lastname + " Family : ";
     for (Person m : members) {
@@ -34,19 +41,25 @@ struct Family {
     return buildon;
   }
 
+  // Constructor
   Family(std::list<Person> mmbrs, string ln) :
     members(mmbrs), lastname(ln) { }
 };
 
+// Child Object -- is a person
 struct Child : Person {
   public:
+  // List of parents
   std::list<Person> parents;
+  // Their family
   Family family;
 
+  // used to add a parent if a parent list does not exist
   void addParent(Person one) {
     parents.push_back(one);
   }
 
+  // Constructors
   Child(string nm, int page, std::list<Person> prts, Family fam) :
   parents(prts), family(fam), Person(nm,page) { toString = "Child : " + name + " - " + to_string(age); }
   Child(string nm, int page, Family fam) :
@@ -54,15 +67,19 @@ struct Child : Person {
 
 };
 
+// Parent Object -- is a person
 struct Parent : Person {
   public:
+  // List of their children
   std::list<Child> children;
   Person partner;
 
+  // Add a child
   void addChildren(Child ch) {
     children.push_back(ch);
   }
 
+  // constructors
   Parent(string nm, int page, std::list<Child> chirren, Person part) :
     Person(nm,page), children(chirren), partner(part) { toString = "Parent : " + name + " - " + to_string(age); }
   Parent(string nm, int page, Person part) :
@@ -70,17 +87,27 @@ struct Parent : Person {
 
 };
 
-
+// Main Program
 int main() {
+  // Make a new person names karen
   Person karen("Karen", 33);
+  // Make a new Parent jeff and set partner to karen
   Parent jeff("Jeff", 34, karen);
+  // now that jeff is karens partner set karen as a parent
   karen = Parent("Karen", 33, jeff);
+  // add them to a list -- edit make the list
   std::list<Person> ashParents;
+  // actually add them to the list
   ashParents.push_back(karen);
   ashParents.push_back(jeff);
+  // Make a new family "Ash Family"
   Family ashFam(ashParents, "Ash");
-  Child shark("Sharqeshia", 12, ashFam);
+  // Make a new child
+  Child shark("Sharqeshia", 12, ashParents, ashFam);
+  // Add them to the family
   ashFam.addMember(shark);
+  // Print out results
   cout << ashFam.toString() << endl;
   return 0;
+  // END
 }
