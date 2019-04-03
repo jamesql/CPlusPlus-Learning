79ae878d64f422fac1b7e04dc6595e3c5b24db50
James <42484571+smitteh1@users.noreply.github.com>
2019-04-03 10:22:33 -0400

    Added power method

--- Recursion.cpp
+++ Recursion.cpp
@@ -7,6 +7,9 @@ using namespace std;
 // Fib Numbers to output
 int fibNumbers = 20;
 
+// Powers base & exponent
+int base = 3;
+int expon = 4;
 // Config of output
 
 // Recursive fib method
@@ -18,11 +21,21 @@ int fib(int n) {
         else return fib(n-1) + fib(n-2);
 }
 
+// Recursive Power method
+int power(int base, int expo) {
+    if (expo == 1)
+      return base;
+      else if (expo == 2)
+            return base * base; 
+             else return base * power(base,expo-1);
+}
+
 // Program
 int main() {
   for (int x = 0; x < fibNumbers; x++)
     cout << fib(x) << " , ";
-
+  cout << endl;
+  cout << "Power : " << power(base,expon) << endl;
   cout << endl;
   return 0;
 }
