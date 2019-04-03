3cd98cc1d29fe2026741d3e21d61e92d9430cacd
James <42484571+smitteh1@users.noreply.github.com>
2019-04-03 08:24:15 -0400

    Update Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+// Config of output
+
+// Fib Numbers to output
 int fibNumbers = 20;
 
+// Config of output
+
+// Recursive fib method
 int fib(int n) {
   if (n == 0)
     return 0;
@@ -12,6 +18,7 @@ int fib(int n) {
         else return fib(n-1) + fib(n-2);
 }
 
+// Program
 int main() {
   for (int x = 0; x < fibNumbers; x++)
     cout << fib(x) << " , ";
