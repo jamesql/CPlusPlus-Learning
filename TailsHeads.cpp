3dc3f8e3262a7d59e03c1b902ada2f023f3cfecb
James <contactsmitteh@gmail.com>
2019-09-10 11:48:58 -0400

    Create TailsHeads.cpp

---
+++ TailsHeads.cpp
@@ -0,0 +1,80 @@
+// Example program
+#include <iostream>
+#include <string>
+#include "list"
+
+using namespace std;
+
+int gamesToBePlayed;
+
+struct game {
+    public:
+    int winner;
+    
+    string getWinningString() {
+        if (winner == 0) {
+            return "Tails Win!";
+        } else {
+            return "Heads Win!";
+        }
+    }
+    
+    game(int win) : winner(win) {}
+
+};
+
+struct gameHistory {
+    public:
+    std::list<game> games;
+    int tails = 0;
+    int heads = 0;
+    double perTails;
+    double perHeads;
+    void addHistory(game history) {
+        games.push_back(history);
+    }
+    void toString() {
+        for (game g : games) {
+        cout << g.getWinningString() << endl;
+        }
+    }
+    std::list<game> getHistoryList() {
+        return games;
+    }
+    
+    gameHistory(game thisGame)
+    {
+     addHistory(thisGame);
+    }
+    gameHistory() {}
+    
+};
+
+gameHistory gh = gameHistory();
+
+game playGame() {
+    int winner = (rand() % 2);
+    game f = game(winner);
+    return f;
+}
+
+
+int main()
+{
+    // seeding
+    srand ( time(NULL) );
+
+  int gamestoget;
+  cout << "How many games to record? ";
+  cin >> gamestoget;
+  cout << "Running " << gamestoget << " Games!\n";
+  gamesToBePlayed = gamestoget;
+  for (int x = 0; x < gamesToBePlayed; x++) {
+      game newGame = playGame();
+      gh.addHistory(newGame);
+  }
+  gh.toString();
+  
+   
+  
+}
